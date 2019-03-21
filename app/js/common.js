b1f11fc2dd24aa0e07b23f70969890b17e08768b
Ilya <pw.i.like@mail.ru>
2019-03-21 19:43:30 +0300

    da

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,9 @@
 $(function() {
-
-	// Custom JS
+	$('.event__slider').slick({
+		dots: false,
+	  	focusOnSelect: true,
+	  	prevArrow:"<i class='slick-prev prev' aria-hidden='true'></i>",
+        nextArrow:"<i class='slick-next next' aria-hidden='true'></i>"
+	});
 
 });
