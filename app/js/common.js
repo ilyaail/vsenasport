64b3cc97da2c5d18b8c4fd1d1f1ebfd9776341ac
Ilya <pw.i.like@mail.ru>
2019-03-21 22:19:05 +0300

    туц

--- app/js/common.js
+++ app/js/common.js
@@ -6,11 +6,30 @@ $(function() {
         nextArrow:"<i class='slick-next next' aria-hidden='true'></i>"
 	});
 
-$(document).ready(function(){
-$('.event__slider').slick({
-	arrows: true
-})
-});
+
+
+var countHamb = 1;
+	$('#hamburger-menu').click(function() {
+		if(countHamb == 1) {
+			countHamb = 0;
+			$('.hamburger').addClass('is-active');
+			$('.header__nav').addClass('active');
+			$('.head-menu').animate({width:'toggle'},350);
+			
+			$("body").css("overflow","hidden"); 
+			
+			console.log('2')
+
+		} else if (countHamb == 0){
+			countHamb = 1;
+			$('.hamburger').removeClass('is-active');
+			$('.head-menu').animate({width:'toggle'},350);
+			$('.header__nav').removeClass('active');
+			$("body").css("overflow","auto");
+			console.log('1')
+		}
 		
+	})
+
 
 });
