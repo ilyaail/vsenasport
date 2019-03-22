ebf07f56e41ca4071e9a56362b00ad88a9756fe2
Ilya <pw.i.like@mail.ru>
2019-03-22 18:57:52 +0300

    city

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function() {
 	$('.event__slider').slick({
 		dots: false,
@@ -50,54 +49,13 @@ $(document).ready(function()
 	$('.header__logo .mobile-logo').attr('src', 'img/logo_calendar_mobile.png');
 }
    
-
-});
-
-=======
-$(function() {
-	$('.event__slider').slick({
-		dots: false,
-	  	focusOnSelect: true,
-	  	prevArrow:"<i class='slick-prev prev' aria-hidden='true'></i>",
-        nextArrow:"<i class='slick-next next' aria-hidden='true'></i>"
-	});
-
-
-
-var countHamb = 1;
-	$('#hamburger-menu').click(function() {
-		if(countHamb == 1) {
-			countHamb = 0;
-			$('.hamburger').addClass('is-active');
-			$('.header__nav').addClass('active');
-			$('.head-menu').animate({width:'toggle'},350);
-			
-			$("body").css("overflow","hidden"); 
-			
-			console.log('2')
-
-		} else if (countHamb == 0){
-			countHamb = 1;
-			$('.hamburger').removeClass('is-active');
-			$('.head-menu').animate({width:'toggle'},350);
-			$('.header__nav').removeClass('active');
-			$("body").css("overflow","auto");
-			console.log('1')
-		}
-		
-	})
-
-
-});
-
-$(document).ready(function()
-{
-	if ($(window).width() < 440) {
+if ($(window.location.pathname == "/city.html") && ($(window).width() < 640) ) {
+    
 		/* заменa логотипа */
-		console.log('less then 440');
-	$('.header__logo .mobile-logo').attr('src', 'img/mobile-logo.png');
+		console.log('less then 640');
+	$('.header__logo .mobile-logo').attr('src', 'img/logo_calendar_mobile.png');
 }
+   
 
+});
 
-	});
->>>>>>> f8a5fb6fc1971115d609d88e94680f07d4b28d57
